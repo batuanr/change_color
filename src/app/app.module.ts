2f1675a5f4b131e5ff7e9f793c847c10c42db902
batuanr <tuanpesi@gmail.com>
2021-12-26 19:52:48 +0700

    xong

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ColorComponent } from './color/color.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ColorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/color/color.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.scss']
+})
+export class ColorComponent implements OnInit {
+  public color: String | undefined;
+  constructor() { }
+  setColor(color: any){
+    this.color = color;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
